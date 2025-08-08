3c7512b2037ee8bd7b260a86e12238c5e1574412
Aashish Shukla <aks1092002@gmail.com>
2025-08-08 17:00:30 +0530

    minor error

--- backend/src/middlewares/oauth.middleware.js
+++ backend/src/middlewares/oauth.middleware.js
@@ -13,7 +13,7 @@ dotenv.config({
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/api/v1/auth/google/callback"
+    callbackURL: "https://oauthbackend-p7y5.onrender.com/api/v1/auth/google/callback"
 }, async (accessToken, refreshToken, profile, done) => {
     try {
         // Check if user already exists
